717916f03177debe62cd0d81394700e98320dc18
lgiacalo <lgiacalo@student.42.fr>
2020-12-07 20:45:42 +0100

    graf: instance en cours

---
+++ instance/js/app.js
@@ -0,0 +1,25 @@
+// let vm = new Vue({
+//   el: "#app",
+//   data: {
+//     message: "Salut les gens",
+//     link: "https://fr.vuejs.org",
+//     success: true,
+//     cls: "success",
+//     persons: ["Jonathan", "Marion", "Marine", "Jean"],
+//   },
+//   methods: {
+//     close: function () {
+//       this.success = false;
+//       // this.message = "close message";
+//     },
+//     style: function () {
+//       if (this.success) return { background: "#0f0" };
+//       else return { background: "#f00" };
+//     },
+//     addPerson: function () {
+//       this.persons.push("Marion");
+//     },
+//   },
+// });
+
+let vm = new Vue({});
