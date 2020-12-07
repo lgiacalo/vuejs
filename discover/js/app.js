abc6c3e69828b495105ae669099ed5eef06bc1f8
lgiacalo <lgiacalo@student.42.fr>
2020-12-07 16:35:45 +0100

    graf: decouverte ok

---
+++ discover/js/app.js
@@ -0,0 +1,20 @@
+new Vue({
+  el: "#app",
+  data: {
+    message: "Salut les gens",
+    link: "https://fr.vuejs.org",
+    success: true,
+    cls: "success",
+    persons: ["Jonathan", "Marion", "Marine", "Jean"],
+  },
+  methods: {
+    close: function () {
+      this.success = false;
+      // this.message = "close message";
+    },
+    style: function () {
+      if (this.success) return { background: "#0f0" };
+      else return { background: "#f00" };
+    },
+  },
+});
