49b0e9a4ef01932c17ecdf5cc50ffee3da16e7d4
lgiacalo <lgiacalo@student.42.fr>
2020-12-09 11:14:42 +0100

    my-first-vue-cli-app: vue add router

--- my-first-vue-cli-app/src/App.vue
+++ my-first-vue-cli-app/src/App.vue
@@ -1,29 +1,12 @@
 <template>
   <div id="app">
-    <nav>
-      <NavLink name="Accueil" url="/accueil" />
-      <NavLink name="A propos" url="/about" />
-      <NavLink
-        v-bind="{
-          name: 'Contact',
-          url: '/contact',
-        }"
-      />
-    </nav>
-    <p>Bienvenu sur notre page d'<a href="/">Accueil</a></p>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script>
-import NavLink from "./components/NavLink.vue";
-
-export default {
-  name: "App",
-  components: {
-    NavLink,
-  },
-};
-</script>
 
 <style lang="scss">
 #app {
@@ -32,6 +15,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
