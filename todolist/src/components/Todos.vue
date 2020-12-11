1f5c6b70a5bd6e7354af7d8477318ee537a76acf
lgiacalo <lgiacalo@student.42.fr>
2020-12-11 17:58:10 +0100

    add new todo + vfor todos

--- todolist/src/components/Todos.vue
+++ todolist/src/components/Todos.vue
@@ -2,13 +2,50 @@
   <section class="todoapp">
     <header class="header">
       <h1>Todos</h1>
+      <input
+        type="text"
+        class="new-todo"
+        placeholder="Ajouter une tache"
+        v-model="newTodo"
+        @keyup.enter="addTodo"
+      />
     </header>
+    <div class="main">
+      <ul class="todo-list">
+        <li class="todo" v-for="todo in todos" :key="todo.name">
+          <div class="view">
+            <input type="checkbox" v-model="todo.completed" class="toggle" />
+            <label>{{ todo.name }}</label>
+          </div>
+        </li>
+      </ul>
+    </div>
   </section>
 </template>
 
 <script>
 export default {
   name: "Todos",
+  data: function() {
+    return {
+      todos: [
+        {
+          name: "Tache de test",
+          completed: false,
+        },
+      ],
+      newTodo: "",
+    };
+  },
+  methods: {
+    addTodo: function() {
+      this.todos.push({
+        completed: false,
+        name: this.newTodo,
+      });
+      this.newTodo = "";
+    },
+  },
 };
 </script>
 
